8eeb2ab12ca529042c8acf0f9531f5b0c7835b80
Buddhima Wijeweera <buddhimawijeweera@Buddhimas-MacBook-Air-2.local>
2026-06-01 16:38:51 +1200

    Add dev prod GitHub deployment pipeline

--- bin/aws-app.ts
+++ bin/aws-app.ts
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { AwsAppStack } from '../lib/aws-app-stack';
+import * as cdk from "aws-cdk-lib";
+import { AwsAppStack, StageName } from "../lib/aws-app-stack";
 
 const app = new cdk.App();
-new AwsAppStack(app, 'AwsAppStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const stageName = process.env.DEPLOY_ENV;
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+if (stageName !== "dev" && stageName !== "prod") {
+  throw new Error("DEPLOY_ENV must be set to either 'dev' or 'prod'");
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new AwsAppStack(app, `InventoryApp-${stageName}`, {
+  stageName: stageName as StageName,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
