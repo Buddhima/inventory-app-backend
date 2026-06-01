6e8e5e609498d9823308a3bd5139d8786a766151
Buddhima Wijeweera <buddhimawijeweera@Buddhimas-MacBook-Air-2.local>
2026-06-01 16:59:15 +1200

    fix: revert external param reference

--- lib/aws-app-stack.ts
+++ lib/aws-app-stack.ts
@@ -56,14 +56,14 @@ export class AwsAppStack extends cdk.Stack {
     const wfmConfigParam = ssm.StringParameter.fromStringParameterName(
       this,
       "WfmConfigParam",
-      `/inventory-app/${stageName}/wfm_token`
+      `/inventory-app/wfm_token`
     );
 
     // SSCC Number postfix
     const ssccPostfixParam = ssm.StringParameter.fromStringParameterName(
       this,
       "SsccPostfixParam",
-      `/inventory-app/${stageName}/sscc_number_postfix`
+      `/inventory-app/sscc_number_postfix`
     );
 
     // Lambda Layer
